1f1c520520694b9ce8d43876eccc36a9d294d6f0
ken-rmndbn <remandabankenneth29@gmail.com>
2026-04-28 02:12:46 +0800

    creating code to save and process output in the file

--- p4_square_cube_of_number/power_seperator.py
+++ p4_square_cube_of_number/power_seperator.py
@@ -9,3 +9,7 @@ class PowerSeperator:
                 self.numbers = [int(line.strip()) for line in file if line.strip()]
         except FileNotFoundError:
             print(f"Error! {self.input_file} not found.")
+
+    def process_and_save(self):
+        if not self.numbers:
+            return
