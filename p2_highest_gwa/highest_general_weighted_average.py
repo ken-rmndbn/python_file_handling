6aa0b3ae564f383e19b688c7af39e546debcd0b3
ken-rmndbn <remandabankenneth29@gmail.com>
2026-04-28 00:31:02 +0800

    seperating name from gwa

--- p2_highest_gwa/highest_general_weighted_average.py
+++ p2_highest_gwa/highest_general_weighted_average.py
@@ -3,3 +3,11 @@ class HighestGeneralWeightedAverage:
         self.filename = filename
         self. top_student = ""
         self.highest_gwa = 3.0
+
+    def find_highest_gwa(self):
+        try:
+            with open(self.filename, "r") as file:
+                for line in file:
+                    parts = line.split()
+                    if len(parts) >= 2:
+                        name = parts[0]
