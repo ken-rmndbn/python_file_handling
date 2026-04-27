046419062f6754657db714ce0dd8e04296b693d7
ken-rmndbn <remandabankenneth29@gmail.com>
2026-04-27 17:45:48 +0800

    reads number from the text file into list

--- p1_even_odd_seperator/even_odd_seperator.py
+++ p1_even_odd_seperator/even_odd_seperator.py
@@ -2,3 +2,7 @@ class EvenOddSeperator:
     def __init__(self, input_filename):
         self.input_filename = input_filename
         self.numbers = []
+
+    def read_number(self):
+        try:
+            with open(self.input_filename),
