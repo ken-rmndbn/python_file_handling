5b2d433b31d4cfe68a4e0c602ef9b5458d7f56ea
ken-rmndbn <remandabankenneth29@gmail.com>
2026-04-28 01:39:52 +0800

    creating code to overwrite file

--- p3_write_multiple_line/multiple_line_writing.py
+++ p3_write_multiple_line/multiple_line_writing.py
@@ -1,3 +1,7 @@
 class MultipleLineWriting:
     def __init__(self, filename):
         self.filename = filename
+
+    def write_entries(self):
+        with open(self.filename, "w") as file:
+            
