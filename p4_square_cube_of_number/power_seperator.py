9446dda1a9ce29454705c48a5d611d40a1a02797
ken-rmndbn <remandabankenneth29@gmail.com>
2026-04-28 02:06:19 +0800

    read each line and convert to integer

--- p4_square_cube_of_number/power_seperator.py
+++ p4_square_cube_of_number/power_seperator.py
@@ -2,4 +2,10 @@ class PowerSeperator:
     def __init__(self, input_file):
         self.input_file = input_file
         self.numbers = []
-        
+
+    def read_integers(self):
+        try:
+            with open(self.input_file, "r") as file:
+                self.numbers = [int(line.strip()) for line in file if line.strip()]
+        except FileNotFoundError:
+            print(f"Error! {self.input_file} not found.")
